d7bf738b1237bd12399037d8dce72ea23fc95bbe
ChandraSagarDev <chandrasagar890@gmail.com>
2024-04-05 19:17:10 +0530

    View Recipies Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './components/AddRecipe';
 import SearchRecipe from './components/SearchRecipe';
+import ViewRecipies from './components/ViewRecipies';
 
 function App() {
   return (
     <div>
     <AddRecipe />
     <SearchRecipe/>
+    <ViewRecipies/>
     </div>
   );
 }
