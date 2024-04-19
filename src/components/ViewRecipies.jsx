31e87b8ce31033e4d3b7905f7374cd12c4492245
ChandraSagarDev <chandrasagar890@gmail.com>
2024-04-20 01:06:26 +0530

    Updated with Dynamic Data

--- src/components/ViewRecipies.jsx
+++ src/components/ViewRecipies.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const ViewRecipies = () => {
+    const [data, changeData] = useState(
+        [
+            { "RecipeTitle": "Crispy Spring Rolls", "Desc": "A Popular Chinese snack of crispy rolls filled with a savory mixed vegetables stuffing.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2015/10/spring-rolls-recipe-280x280.jpg" },
+            { "RecipeTitle": "Paneer Tikka ", "Desc": "Paneer are marinated in a spiced yogurt-based marinade, arranged on skewers and grilled in the oven.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2011/10/grilled-paneer-tikka-280x280.jpg" },
+            { "RecipeTitle": "Classic Punjabi Samosa ", "Desc": "One of the most popular street food snack filled with savory and spiced potato and green peas.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2013/11/samosa-0-280x280.jpg" },
+            { "RecipeTitle": "Cheesy Pizza ", "Desc": "One of the most fun and easy dinners to make at home with just a few key tips.", "Category": "Non-Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2020/11/pizza-recipe-280x280.jpg" },
+            { "RecipeTitle": "Mysore Masala Dosa ", "Desc": "It is a crispy, thin savory pancake with or without the humble potato filling or masala. Mysore Masala Dosa is one of the most popular ones.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2013/05/mysore-masala-dosa-recipe-280x280.jpg" },
+            { "RecipeTitle": "Rava Kesari ", "Desc": "Basically, its fine semolina which is cooked with ghee, sugar and flavorings till a halwa like consistency is achieved. This Rava Kesari With Milk.", "Category":"Veg", "By":"Dassana Amit", "Img":"https://www.vegrecipesofindia.com/wp-content/uploads/2015/04/milk-kesari-recipe-280x280.jpg"},
+            { "RecipeTitle": "Crispy Spring Rolls", "Desc": "A Popular Chinese snack of crispy rolls filled with a savory mixed vegetables stuffing.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2015/10/spring-rolls-recipe-280x280.jpg" },
+            { "RecipeTitle": "Paneer Tikka ", "Desc": "Paneer are marinated in a spiced yogurt-based marinade, arranged on skewers and grilled in the oven.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2011/10/grilled-paneer-tikka-280x280.jpg" },
+            { "RecipeTitle": "Classic Punjabi Samosa ", "Desc": "One of the most popular street food snack filled with savory and spiced potato and green peas.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2013/11/samosa-0-280x280.jpg" },
+            { "RecipeTitle": "Cheesy Pizza ", "Desc": "One of the most fun and easy dinners to make at home with just a few key tips.", "Category": "Non-Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2020/11/pizza-recipe-280x280.jpg" },
+            { "RecipeTitle": "Mysore Masala Dosa ", "Desc": "It is a crispy, thin savory pancake with or without the humble potato filling or masala. Mysore Masala Dosa is one of the most popular ones.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2013/05/mysore-masala-dosa-recipe-280x280.jpg" },
+            { "RecipeTitle": "Rava Kesari ", "Desc": "Basically, its fine semolina which is cooked with ghee, sugar and flavorings till a halwa like consistency is achieved. This Rava Kesari With Milk.", "Category":"Veg", "By":"Dassana Amit", "Img":"https://www.vegrecipesofindia.com/wp-content/uploads/2015/04/milk-kesari-recipe-280x280.jpg"},
+            { "RecipeTitle": "Crispy Spring Rolls", "Desc": "A Popular Chinese snack of crispy rolls filled with a savory mixed vegetables stuffing.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2015/10/spring-rolls-recipe-280x280.jpg" },
+            { "RecipeTitle": "Paneer Tikka ", "Desc": "Paneer are marinated in a spiced yogurt-based marinade, arranged on skewers and grilled in the oven.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2011/10/grilled-paneer-tikka-280x280.jpg" },
+            { "RecipeTitle": "Classic Punjabi Samosa ", "Desc": "One of the most popular street food snack filled with savory and spiced potato and green peas.", "Category": "Veg", "By": "Dassana Amit", "Img": "https://www.vegrecipesofindia.com/wp-content/uploads/2013/11/samosa-0-280x280.jpg" }
+        ]
+    )
+    
     return (
         <div>
             <Navbar />
@@ -9,117 +29,32 @@ const ViewRecipies = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                <div class="card">
-                                    <img src="https://www.vegrecipesofindia.com/wp-content/uploads/2015/10/spring-rolls-recipe-280x280.jpg" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Crispy Spring Rolls</h5>
-                                        <p class="card-text">A Popular Chinese snack of crispy rolls filled with a savory mixed vegetables stuffing.</p>
+                            {data.map(
+                                (value, index) => {
+                                    return <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                        <div class="card">
+                                            <img src={value.Img} class="card-img-top" alt="..." />
+                                            <div class="card-body">
+                                                <h5 class="card-title">{value.RecipeTitle}</h5>
+                                                <p class="card-text">{value.Desc}</p>
+                                            </div>
+                                            <ul class="list-group list-group-flush">
+                                                <li class="list-group-item">{value.Category}</li>
+                                                <li class="list-group-item">{value.By}</li>
+                                            </ul>
+                                            <div class="card-body">
+                                                <a href="#" class="card-link">Ingredients</a>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Veg</li>
-                                        <li class="list-group-item">By Dassana Amit</li>
-                                    </ul>
-                                    <div class="card-body">
-                                        <a href="#" class="card-link">Ingredients</a>
-                                    </div>
-                                </div>
+                                }
+                            )}
 
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                <div class="card">
-                                    <img src="https://www.vegrecipesofindia.com/wp-content/uploads/2011/10/grilled-paneer-tikka-280x280.jpg" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Paneer Tikka</h5>
-                                        <p class="card-text">Paneer are marinated in a spiced yogurt-based marinade, arranged on skewers and grilled in the oven.</p>
-                                    </div>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Veg</li>
-                                        <li class="list-group-item">By </li>
-                                    </ul>
-                                    <div class="card-body">
-                                        <a href="#" class="card-link">Ingredients</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://www.vegrecipesofindia.com/wp-content/uploads/2013/11/samosa-0-280x280.jpg" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Classic Punjabi Samosa</h5>
-                                        <p class="card-text">One of the most popular street food snack filled with savory and spiced potato and green peas.</p>
-                                    </div>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Veg</li>
-                                        <li class="list-group-item">By</li>
-                                    </ul>
-                                    <div class="card-body">
-                                        <a href="#" class="card-link">Ingredients</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://www.vegrecipesofindia.com/wp-content/uploads/2020/11/pizza-recipe-280x280.jpg" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Cheesy Pizza</h5>
-                                        <p class="card-text">One of the most fun and easy dinners to make at home with just a few key tips.</p>
-                                    </div>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Non Veg</li>
-                                        <li class="list-group-item">By</li>
-                                    </ul>
-                                    <div class="card-body">
-                                        <a href="#" class="card-link">Ingredients</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://www.vegrecipesofindia.com/wp-content/uploads/2013/05/mysore-masala-dosa-recipe-280x280.jpg" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Mysore Masala Dosa</h5>
-                                        <p class="card-text"> It is a crispy, thin savory pancake with or without the humble potato filling or masala. Mysore Masala Dosa is one of the most popular ones.</p>
-                                    </div>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Veg</li>
-                                        <li class="list-group-item">By</li>
-                                    </ul>
-                                    <div class="card-body">
-                                        <a href="#" class="card-link">Ingredients</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div class="card">
-                                    <img src="https://www.vegrecipesofindia.com/wp-content/uploads/2015/04/milk-kesari-recipe-280x280.jpg" class="card-img-top" alt="..." />
-                                    <div class="card-body">
-                                        <h5 class="card-title">Rava Kesari</h5>
-                                        <p class="card-text">Basically, its fine semolina which is cooked with ghee, sugar and flavorings till a halwa like consistency is achieved. This Rava Kesari With Milk.</p>
-                                    </div>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Veg</li>
-                                        <li class="list-group-item">By</li>
-                                    </ul>
-                                    <div class="card-body">
-                                        <a href="#" class="card-link">Ingredients</a>
-                                    </div>
-                                </div>
-
-                            </div>
 
                         </div>
-<br />
+                        <br />
                         <div className="row g-3">
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                                 <nav aria-label="Page navigation example">
